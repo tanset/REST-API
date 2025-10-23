46996d025da79a1cd4e425403f93f4820238c08d
tanset <haoyuchang8@gmail.com>
2025-10-23 13:34:14 -0400

    implement get post db

--- main.go
+++ main.go
@@ -1,6 +1,35 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tanset/REST-API/models"
+)
 
 func main() {
-	
+	server := gin.Default()
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+	server.Run(":8080")
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindBodyWithJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	event.Save()
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
