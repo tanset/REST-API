9401abca0ff2c317f4d0cb4ca12deb6d9b4f4b45
tanset <haoyuchang8@gmail.com>
2025-10-24 23:05:01 -0400

    implement routes for insert, select, update, and delete

--- main.go
+++ main.go
@@ -1,35 +1,16 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
-	"github.com/tanset/REST-API/models"
+	"github.com/tanset/REST-API/db"
+	"github.com/tanset/REST-API/routes"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
-	server.GET("/events", getEvents)
-	server.POST("/events", createEvent)
+	routes.RegisterRoutes(server)
 	server.Run(":8080")
 }
 
-func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
-	context.JSON(http.StatusOK, events)
-}
 
-func createEvent(context *gin.Context) {
-	var event models.Event
-	err := context.ShouldBindBodyWithJSON(&event)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
-		return
-	}
-
-	event.ID = 1
-	event.UserID = 1
-	event.Save()
-	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
-}
